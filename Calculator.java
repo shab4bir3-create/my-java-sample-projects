9c56e320c39d649c762a5b82c37a45e544ac7d9f
Shabbir Shaik <shabbirshk3@gmail.com>
2026-01-11 12:01:53 -0600

    added Calculator program

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+package com.abcbank.credicard;
+ 
+public class Calculator {
+ 
+	
+	public int add (int a, int b) {
+		int addResult = a+b;
+		return addResult;
+	}
+	public int sub(int a , int b) {
+		int subResult = a-b;
+	  return subResult;	
+	}
+	public int mul(int a, int b) {
+		int mulResult = a*b;
+		return mulResult;
+	}
+	public int  div(int a, int b) {
+		int divResult = a/b;
+		return divResult;
+	}
+}
